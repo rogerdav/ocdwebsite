eb20270f16e9448ff040741053891531e40e8c7f
Roger <roger.neil.davenport@gmail.com>
2018-10-23 21:41:04 -0700

    added resume to send email

--- src/sendemail.js
+++ src/sendemail.js
@@ -6,6 +6,7 @@ aws.config = ({ "accessKeyId": process.env.REACT_APP_AWS_KEY_ID, "secretAccessKe
 
 // this function send the informatin from the form to email reciepient
 module.exports = function(formInfo) {
+  // console.log('formInfo at top of send email', formInfo.resume)
 
 // This address must be verified with Amazon SES.
   const sender = 'Sender Name <roger.neil.davenport@gmail.com>';
@@ -14,25 +15,34 @@ module.exports = function(formInfo) {
   const subject = `${formInfo.subject} `;
 
   // The email body for recipients with non-HTML email clients.
-  const body_text = 'Request for quote this is the body of the message';
-            
-  // The HTML body of the email.
-  // console.log("body resume after map", body_resume);
-  const body_html = `<html>
+  const body_text = `${formInfo.subject}`;
+  let body_resume;
+  if (formInfo.subject === 'Job Application') {
+    body_resume = `<li>Resume from ${formInfo.name}</li>`;
+    body_resume = formInfo.resume.map( item => `<li>${item}</li>`).join("");
+  }
+  if (formInfo.subject === 'Request For Estimate') {
+    body_resume = ``;
+  }
+  
+  
+    const body_html = `<html>
 <head></head>
 <body>
-  <h1>Request for Estimate</h1>
+  <h1>${formInfo.subject}</h1>
   <ul>
-  <li>Name: ${formInfo.name}</li>
-  <li>Email: ${formInfo.email}</li>
-  <li>Phone: ${formInfo.phone}</li>
- <li>Date and Time: ${formInfo.date}</li>
- <li>Message: ${formInfo.message}</li>
- <li>File: ${formInfo.file}</li>
+    <li>Name: ${formInfo.name}</li>
+    <li>Email: ${formInfo.email}</li>
+    <li>Phone: ${formInfo.phone_number}</li>
+    <li>Date and Time: ${formInfo.date}</li>
+    <li>Message:${formInfo.message} </li>
+    <li>--------------------------------------</li>
+    <li>Resume</li>
+    ${body_resume}
   </ul>
- 
 </body>
 </html>`;
+// console.log('html body', body_html);
 
   // The character encoding for the email.
   const charset = 'UTF-8';
@@ -69,15 +79,15 @@ module.exports = function(formInfo) {
   };
   
   //Try to send the email.
-  // ses.sendEmail(params, function(err, data) {
-  //   // If something goes wrong, print an error message.
+  ses.sendEmail(params, function(err, data) {
+    // If something goes wrong, print an error message.
     
-  //   if(err) {
-  //     console.log(err.message);
-  //   } else {
-  //     console.log('Email sent! Message ID: ', data.MessageId);
-  //   }
-  // });
+    if(err) {
+      console.log(err.message);
+    } else {
+      console.log('Email sent! Message ID: ', data.MessageId);
+    }
+  });
 
 
 
