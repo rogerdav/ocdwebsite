befbf72b431fbf3356fab1ba2ae27bc4eb2ce428
Roger <roger.neil.davenport@gmail.com>
2018-10-24 20:41:11 -0700

    added functionality to email call request info

--- src/callmodal.js
+++ src/callmodal.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import sendEmail from './sendemail';
 
 class ModalCall extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      date: new Date().toLocaleString(),
+      name: '',
+      phone_number: '',
+      email: '',
+      time: '',
+      message: '',
+      submitted: false,
+      subject: 'Call Request',
     };
 
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.toggle = this.toggle.bind(this);
+    this.submitCallRequest = this.submitCallRequest.bind(this);
   }
 
   toggle() {
     this.props.onClick();
+  }
+  
+  submitCallRequest() {
+       this.toggle();
+       sendEmail(this.state); 
+       this.setState({ submitted: !this.state.submitted});  
+  }
+
+  handleInputChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+    
   }
 
   render() {
@@ -25,28 +46,28 @@ class ModalCall extends React.Component {
           <Form>
             <FormGroup>
               <Label for="name">Name</Label>
-              <Input type="text" name="name" id="name" placeholder="name required" />
+              <Input type="text" name="name" id="name" placeholder="name required" onChange={this.handleInputChange}/>
             </FormGroup>
             <FormGroup>
               <Label for="phone_number">Phone Number</Label>
-              <Input type="text" name="phone_number" id="phone_number" placeholder="phone number required" />
+              <Input type="text" name="phone_number" id="phone_number" placeholder="phone number required" onChange={this.handleInputChange} />
             </FormGroup>
             <FormGroup>
               <Label for="email">Email</Label>
-              <Input type="email" name="email" id="email" placeholder="email optional" />
+              <Input type="email" name="email" id="email" placeholder="email optional" onChange={this.handleInputChange}/>
             </FormGroup>
             <FormGroup>
               <Label for="time">Best Time To Call</Label>
-              <Input type="text" name="time" id="time" placeholder="best time to call during business hours" />
+              <Input type="text" name="time" id="time" placeholder="best time to call during business hours" onChange={this.handleInputChange}/>
             </FormGroup>
             <FormGroup>
               <Label for="message">Message</Label>
-              <Input type="textarea" name="message" id="message" placeholder="Any additional info" />
+              <Input type="textarea" name="message" id="message" placeholder="Any additional info" onChange={this.handleInputChange}/>
             </FormGroup>
           </Form>
           </ModalBody>
           <ModalFooter>
-            <Button className="submitbutton"color="primary" onClick={this.toggle}>Submit Request</Button>
+            <Button className="submitbutton"color="primary" onClick={this.submitCallRequest}>Submit Request</Button>
             <Button color="secondary" onClick={this.props.onClick}>Cancel</Button>
           </ModalFooter>
         </Modal>
--- src/sendemail.js
+++ src/sendemail.js
@@ -16,34 +16,55 @@ module.exports = function(formInfo) {
 
   // The email body for recipients with non-HTML email clients.
   const body_text = `${formInfo.subject}`;
-  let body_resume;
+  let body_resume, body_html;
   if (formInfo.subject === 'Job Application') {
     body_resume = `<p>Resume from ${formInfo.name}</p>`;
     body_resume = formInfo.resume.map( item => `<p>${item}</p>`).join("");
-  }
-  if (formInfo.subject === 'Request For Estimate') {
-    body_resume = ``;
+    body_html = 
+    `<html>
+     <head></head>
+     <body>
+     <h1>${formInfo.subject}</h1>
+     <ul>
+        <li>Name: ${formInfo.name}</li>
+        <li>Email: ${formInfo.email}</li>
+        <li>Phone: ${formInfo.phone_number}</li>
+        <li>Eligible to work: ${formInfo.canwork}</li>
+        <li>Date and Time: ${formInfo.date}</li>
+        <li>Message:${formInfo.message} </li>
+        <li>Best Time: ${formInfo.time} </li>
+        <li>--------------------------------------</li>
+        <li><h3>Resume<h3></li>
+      </ul>
+      ${body_resume}
+      </body>
+      </html>`;
+
+    };
+
+  if (formInfo.subject === 'Call Request') {
+    
+    body_html = 
+    `<html>
+     <head></head>
+     <body>
+     <h1>${formInfo.subject}</h1>
+     <ul>
+        <li>Name: ${formInfo.name}</li>
+        <li>Email: ${formInfo.email}</li>
+        <li>Phone: ${formInfo.phone_number}</li>
+        <li>Date and Time: ${formInfo.date}</li>
+        <li>Message:${formInfo.message} </li>
+        <li>Best Time: ${formInfo.time} </li>
+        <li>--------------------------------------</li>
+     </ul>
+      
+      </body>
+      </html>`;
   }
   
   
-    const body_html = `<html>
-<head></head>
-<body>
-  <h1>${formInfo.subject}</h1>
-  <ul>
-    <li>Name: ${formInfo.name}</li>
-    <li>Email: ${formInfo.email}</li>
-    <li>Phone: ${formInfo.phone_number}</li>
-    <li>Eligible to work: ${formInfo.canwork}</li>
-    <li>Date and Time: ${formInfo.date}</li>
-    <li>Message:${formInfo.message} </li>
-    <li>--------------------------------------</li>
-    <li><h3>Resume<h3></li>
-    </ul>
-    ${body_resume}
-</body>
-</html>`;
-// console.log('html body', body_html);
+
 
   // The character encoding for the email.
   const charset = 'UTF-8';
